4c6341854736d90cfbae54c32713624aa48e1d47
NightKosh <nightkosh@gmail.com>
2026-01-27 12:28:06 +0100

    Frozen slimes create snow and melt like snowmans

--- src/main/java/nightkosh/withered_lands/entity/slime/FrozenSlime.java
+++ src/main/java/nightkosh/withered_lands/entity/slime/FrozenSlime.java
@@ -3,8 +3,12 @@ package nightkosh.withered_lands.entity.slime;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.attribute.EnvironmentAttributes;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -16,6 +20,8 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.neoforged.neoforge.event.EventHooks;
 import nightkosh.withered_lands.core.WLConfigs;
 import nightkosh.withered_lands.helper.TimeHelper;
 
@@ -37,6 +43,51 @@ public class FrozenSlime extends ASlime {
     protected void applyEffect(LivingEntity entity) {
         super.applyEffect(entity);
         entity.setTicksFrozen(entity.getTicksFrozen() + TimeHelper.SECONDS_15);
+    }
+
+    @Override
+    public void die(@Nonnull DamageSource damageSource) {
+        super.die(damageSource);
+        if (!this.level().isClientSide() && WLConfigs.MOLTEN_SLIME_LAVA.get() && this.getSize() > 1) {
+            var pos = this.blockPosition();
+
+            if (this.level().getBlockState(pos).getCollisionShape(this.level(), pos).isEmpty() &&
+                    !this.level().getBlockState(pos.below()).is(Blocks.WATER)) {
+                this.level().setBlock(pos, Blocks.POWDER_SNOW.defaultBlockState(), 3);
+            }
+        }
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        if (this.level() instanceof ServerLevel level) {
+            if (level.environmentAttributes().getValue(EnvironmentAttributes.SNOW_GOLEM_MELTS, this.position())) {
+                this.hurtServer(level, this.damageSources().onFire(), 1);
+            }
+
+            if (!EventHooks.canEntityGrief(level, this)) {
+                return;
+            }
+
+            var blockstate = Blocks.SNOW.defaultBlockState();
+            for (int i = 0; i < 4; i++) {
+                var blockpos = new BlockPos(
+                        Mth.floor(this.getX() + (i % 2 * 2 - 1) * 0.25F),
+                        Mth.floor(this.getY()),
+                        Mth.floor(this.getZ() + (i / 2 % 2 * 2 - 1) * 0.25F));
+                if (this.level().getBlockState(blockpos).isAir() && blockstate.canSurvive(this.level(), blockpos)) {
+                    this.level().setBlockAndUpdate(blockpos, blockstate);
+                    this.level().gameEvent(GameEvent.BLOCK_PLACE, blockpos, GameEvent.Context.of(this, blockstate));
+                }
+            }
+        }
+    }
+
+    @Nonnull
+    @Override
+    protected ParticleOptions getParticleType() {
+        return ParticleTypes.SNOWFLAKE;
     }
 
     public static AttributeSupplier createAttributeSupplier() {
@@ -77,12 +128,6 @@ public class FrozenSlime extends ASlime {
         }
 
         return false;
-    }
-
-    @Nonnull
-    @Override
-    protected ParticleOptions getParticleType() {
-        return ParticleTypes.SNOWFLAKE;
     }
 
 }
