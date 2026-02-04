381d80d8a7237d85a591ecc401c74f7ced8f2c73
NightKosh <nightkosh@gmail.com>
2026-02-04 19:27:06 +0100

    Curse of Starvation deals 0.5 damage per item with 20% chance per second

--- src/main/java/nightkosh/withered_lands/helper/WLEnchantmentHelper.java
+++ src/main/java/nightkosh/withered_lands/helper/WLEnchantmentHelper.java
@@ -3,6 +3,7 @@ package nightkosh.withered_lands.helper;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -27,18 +28,23 @@ public class WLEnchantmentHelper {
     }
 
     public static void applyCurseEffect(Player player) {
-        var duration = 0;
+        float damage = 0;
+        int duration = 0;
         var ench = getEnchantmentHolder(player.level(), WLEnchantments.CURSE_OF_STARVATION);
         for (var slot : EquipmentSlot.values()) {
             var item = player.getItemBySlot(slot);
             if (!item.isEmpty()) {
                 if (EnchantmentHelper.getItemEnchantmentLevel(ench, item) > 0) {
+                    damage += 0.5F;
                     duration += TimeHelper.SECONDS_10;
                 }
             }
         }
         if (duration > 0) {
             player.addEffect(new MobEffectInstance(MobEffects.HUNGER, duration, 0));
+            if (player.level() instanceof ServerLevel server && player.getRandom().nextInt(5) == 0) {
+                player.hurtServer(server, server.damageSources().magic(), damage);
+            }
         }
     }
 
